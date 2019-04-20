bea6431ad287291639aa556b168e5487806f7214
beforedream <hust15litao@outlook.com>
2019-04-20 15:05:25 +0800

    删除：     employee/index.html 	删除：     personnel/articlePublish.html 	修改：     personnel/check-perm.php
    	employee/
    	login/
    	personnel/.htaccess
    	personnel/com
    	salary/.htaccess
    	salary/com

--- personnel/check-perm.php
+++ personnel/check-perm.php
@@ -1,10 +1,36 @@
-<!DOCTYPE html> 
-<html> 
-<body> 
+<?php
+class caps{
+    var $EID = -1;
+    var $lastTime = 0;
+    var $timeOut = 300;
+    var $pageName;
+    public function __construct($new_EID, $new_pageName){
+        $this->EID = $new_EID;
+        $this->pageName = $new_pageName;
+        /**
+         * TODO
+         * get some info from database, and fill other parameter;
+         * if not exist echo login page;
+         */
+    }
+    /**
+     * send login page
+     */
+    public function sendPage(){
 
-<?php 
-	phpinfo();
-?> 
+    }
 
-</body> 
-</html>
+    public function updateCap(){
+
+    }
+    /**
+     * if exist return its' perm;
+     * else return 0;
+     * 0-none, 1-read, 2-write, 3-all;
+     */
+    private function checkPerm(){
+
+    }
+}
+$cap = new caps();
+?>
